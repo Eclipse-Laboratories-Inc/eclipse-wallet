3717a9ff41647bfac97afe91fe1d544f23c3dcc0
Pablo Mussari <pablo@captaincode.io>
2022-05-26 19:44:33 +0200

    Modal and Text input on Onboarding

--- src/pages/Onboarding/CreateWallet.js
+++ src/pages/Onboarding/CreateWallet.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import Box from '../../component-library/Box/Box';
 import Button from '../../component-library/Button/Button';
+import FormDialog from '../../component-library/Dialog/FormDialog';
 import TextArea from '../../component-library/Input/TextArea';
+import TextInput from '../../component-library/Input/TextInput';
 import PageLayout from '../../component-library/Layout/PageLayout';
 import TextParagraph from '../../component-library/Text/TextParagraph';
 import TextTitle from '../../component-library/Text/TextTitle';
@@ -24,37 +26,83 @@ const Message = ({onNext}) => (
   </>
 );
 
-const Form = ({seedPhrase, setSeedPhrase}) => (
-  <>
-    <Box px={10} py={10}>
-      <TextTitle>Keep your seed safe!</TextTitle>
-    </Box>
-    <Box px={10} py={10}>
-      <TextArea
-        label="Seed Words"
-        lines={5}
-        value={seedPhrase}
-        setValue={setSeedPhrase}
-        disabled={true}
-      />
-    </Box>
-    <Box px={10} py={10}>
-      <Button>Download Key</Button>
-    </Box>
-  </>
-);
+const Form = ({seedPhrase, setSeedPhrase, onComplete}) => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <Box px={10} py={10}>
+        <TextTitle>Keep your seed safe!</TextTitle>
+      </Box>
+      <Box px={10} py={10}>
+        <TextArea
+          label="Seed Words"
+          lines={5}
+          value={seedPhrase}
+          setValue={setSeedPhrase}
+        />
+      </Box>
+      <Box px={10} py={10}>
+        <Button>Download Key</Button>
+      </Box>
+      <Box px={10} py={10}>
+        <Button onClick={() => setShowModal(true)}>Continue</Button>
+      </Box>
+      <FormDialog
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+        onSubmit={() => {
+          setShowModal(false);
+          onComplete();
+        }}
+        title="Check"
+        text="Text"
+        labelCancel="Cancel"
+        labelSubmit="Continue">
+        <TextArea
+          label="Seed Words"
+          lines={5}
+          value={seedPhrase}
+          setValue={setSeedPhrase}
+        />
+      </FormDialog>
+    </>
+  );
+};
+
+const Password = ({onComplete}) => {
+  const [pass, setPass] = useState('');
+  const [repass, setRepass] = useState('');
+  return (
+    <>
+      <Box px={10} py={10}>
+        <TextInput label="Password" value={pass} setValue={setPass} />
+      </Box>
+      <Box px={10} py={10}>
+        <TextInput label="Re Password" value={repass} setValue={setRepass} />
+      </Box>
+      <Box px={10} py={10}>
+        <Button onClick={() => onComplete()}>Continue</Button>
+      </Box>
+    </>
+  );
+};
 
 const CreateWallet = () => {
-  const [showMessage, setShowMessage] = useState(true);
+  const [step, setStep] = useState(1);
   const [seedPhrase, setSeedPhrase] = useState(
     'asd asd asdas asd asd asd asd asd asd asd asd',
   );
   return (
     <PageLayout>
-      {showMessage && <Message onNext={() => setShowMessage(false)} />}
-      {!showMessage && (
-        <Form seedPhrase={seedPhrase} setSeedPhrase={setSeedPhrase} />
+      {step === 1 && <Message onNext={() => setStep(2)} />}
+      {step === 2 && (
+        <Form
+          seedPhrase={seedPhrase}
+          setSeedPhrase={setSeedPhrase}
+          onComplete={() => setStep(3)}
+        />
       )}
+      {step === 3 && <Password onComplete={() => setStep(0)} />}
     </PageLayout>
   );
 };
