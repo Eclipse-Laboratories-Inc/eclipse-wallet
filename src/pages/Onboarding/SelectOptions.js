3e3bf9e63d9572c938827cac1a441114941a061a
Pablo Mussari <pablo@captaincode.io>
2022-07-09 16:11:37 +0200

    Adding blockchain selector step

--- src/pages/Onboarding/SelectOptions.js
+++ src/pages/Onboarding/SelectOptions.js
@@ -1,48 +1,80 @@
 import React, { useState } from 'react';
 import Box from '../../component-library/Box/Box';
 import Button from '../../component-library/Button/Button';
+import SelectableCard from '../../component-library/Card/SelectableCard';
+import AvatarImage from '../../component-library/Image/AvatarImage';
 import Image from '../../component-library/Image/Image';
-import PageLayout from '../../component-library/Layout/PageLayout';
-import BasicSelect from '../../component-library/Selects/BasicSelect';
+import GlobalLayout from '../../component-library/Layout/GlobalLayout';
 import TextTitle from '../../component-library/Text/TextTitle';
 import Logo from '../../images/logo.png';
 import { useNavigation } from '../../routes/hooks';
+import { getChains, LOGOS } from '../../utils/wallet';
 import { ROUTES_MAP } from './routes';
+
+const SelectAction = ({ onNext }) => (
+  <>
+    <Box px={10} py={10}>
+      <TextTitle>Welcome to</TextTitle>
+    </Box>
+    <Box px={10} py={10}>
+      <Image src={Logo} alt="logo" />
+    </Box>
+    <Box px={10} py={10}>
+      <Button onClick={() => onNext(ROUTES_MAP.ONBOARDING_CREATE)}>
+        Crear wallet
+      </Button>
+    </Box>
+    <Box px={10} py={10}>
+      <Button onClick={() => onNext(ROUTES_MAP.ONBOARDING_RECOVER)}>
+        Recuperar wallet
+      </Button>
+    </Box>
+  </>
+);
+
+const SelectChain = ({ onNext, blockChains }) => {
+  const [selected, setSelected] = useState(0);
+
+  return (
+    <>
+      <Box px={10} py={10}>
+        <TextTitle>Select Blockchain</TextTitle>
+      </Box>
+      <Box px={10} py={10}>
+        {blockChains.map((chain, index) => (
+          <SelectableCard
+            selected={index === selected}
+            onSelect={() => setSelected(index)}
+            headerIcon={<AvatarImage url={LOGOS[chain]} size={48} />}
+            headerTitle={chain}
+          />
+        ))}
+      </Box>
+      <Box px={10} py={10}>
+        <Button onClick={() => onNext(blockChains[selected])}>Continue</Button>
+      </Box>
+    </>
+  );
+};
 
 const SelectOptions = () => {
   const navigate = useNavigation();
-  const [chainCode, setChainCode] = useState('');
+  const [actionRoute, setActionRoute] = useState();
+  const [step, setStep] = useState(0);
+  const onSelectAction = action => {
+    setActionRoute(action);
+    setStep(1);
+  };
+  const onSelectChain = chain => {
+    navigate(actionRoute);
+  };
   return (
-    <PageLayout>
-      <Box px={10} py={10}>
-        <Image src={Logo} alt="logo" />
-      </Box>
-      <Box px={10} py={10}>
-        <TextTitle>Welcome</TextTitle>
-      </Box>
-      <Box px={10} py={10}>
-        <BasicSelect
-          value={chainCode}
-          setValue={setChainCode}
-          label="Chain"
-          options={[{ value: 'SOLANA', label: 'Solana' }]}
-        />
-      </Box>
-      <Box px={10} py={10}>
-        <Button
-          onClick={() => navigate(ROUTES_MAP.ONBOARDING_CREATE)}
-          disabled={!chainCode}>
-          Crear wallet
-        </Button>
-      </Box>
-      <Box px={10} py={10}>
-        <Button
-          onClick={() => navigate(ROUTES_MAP.ONBOARDING_RECOVER)}
-          disabled={!chainCode}>
-          Recuperar wallet
-        </Button>
-      </Box>
-    </PageLayout>
+    <GlobalLayout>
+      {step === 0 && <SelectAction onNext={onSelectAction} />}
+      {step === 1 && (
+        <SelectChain onNext={onSelectChain} blockChains={getChains()} />
+      )}
+    </GlobalLayout>
   );
 };
 
