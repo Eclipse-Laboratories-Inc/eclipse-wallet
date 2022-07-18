964d6e750085f53291fcb724e98e3e7340bf1329
Mauricio Grijalba <grijos@gmail.com>
2022-07-18 16:11:51 -0300

    Design: GlobalImage in NtfsList

--- src/features/NtfsList/NtfsList.js
+++ src/features/NtfsList/NtfsList.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import Card from '../../component-library/Card/Card';
+import { StyleSheet, View } from 'react-native';
+
+import theme from '../../component-library/Global/theme';
+import GlobalImage from '../../component-library/Global/GlobalImage';
+
 import Grid from '../../component-library/Grid/Grid';
 import { getMediaRemoteUrl } from '../../utils/media';
 
+const styles = StyleSheet.create({
+  image: {
+    aspectRatio: 1,
+    backgroundColor: theme.colors.bgLight,
+    borderRadius: theme.borderRadius.borderRadiusXL,
+    overflow: 'hidden',
+  },
+});
+
 const NtfsList = ({ ntfs }) => (
   <Grid
-    spacing={0.5}
+    spacing={1}
     columns={2}
-    items={ntfs.map(ntf => (
-      <Card key={ntf.url} media={getMediaRemoteUrl(ntf.image)} />
-    ))}
+    items={ntfs.map(ntf => {
+      console.log(getMediaRemoteUrl(ntf.image));
+      return (
+        <View style={styles.image}>
+          <GlobalImage
+            key={ntf.url}
+            source={getMediaRemoteUrl(ntf.image).url}
+            size="block"
+          />
+        </View>
+      );
+    })}
   />
 );
 
