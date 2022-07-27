b4b48c9ff784ab8fe588dded7451dd812c4c7798
Mauricio Grijalba <grijos@gmail.com>
2022-07-27 19:16:20 -0300

    Design: Address Book List New Edit

---
+++ src/pages/Settings/AddressBookPage.js
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react';
+
+import { AppContext } from '../../AppProvider';
+import { useNavigation } from '../../routes/hooks';
+import { ROUTES_MAP } from './routes';
+import { withTranslation } from '../../hooks/useTranslations';
+
+import GlobalLayout from '../../component-library/Global/GlobalLayout';
+import GlobalBackTitle from '../../component-library/Global/GlobalBackTitle';
+import GlobalButton from '../../component-library/Global/GlobalButton';
+import CardButtonWallet from '../../component-library/CardButton/CardButtonWallet';
+
+const AddressBookPage = ({ t }) => {
+  const navigate = useNavigation();
+
+  const [{ addressBook }] = useContext(AppContext);
+
+  const onBack = () => navigate(ROUTES_MAP.SETTINGS_OPTIONS);
+
+  const goToAddNewAddressBook = () =>
+    navigate(ROUTES_MAP.SETTINGS_ADDRESSBOOK_ADD);
+
+  const onEditAddressBook = ({ address }) =>
+    navigate(ROUTES_MAP.SETTINGS_ADDRESSBOOK_EDIT, { address });
+
+  return (
+    <GlobalLayout>
+      <GlobalLayout.Header>
+        <GlobalBackTitle
+          onBack={onBack}
+          title={t('settings.addressbook.title')}
+        />
+
+        {addressBook.map(addressBookItem => (
+          <CardButtonWallet
+            key={addressBookItem.address}
+            title={addressBookItem.name}
+            address={addressBookItem.address}
+            chain={addressBookItem.chain}
+            imageSize="md"
+            onPress={() => onEditAddressBook(addressBookItem)}
+          />
+        ))}
+      </GlobalLayout.Header>
+
+      <GlobalLayout.Footer>
+        <GlobalButton
+          type="primary"
+          wideSmall
+          title={t('settings.addressbook.addnew')}
+          onPress={goToAddNewAddressBook}
+        />
+      </GlobalLayout.Footer>
+    </GlobalLayout>
+  );
+};
+
+export default withTranslation()(AddressBookPage);
--- src/pages/Token/TokenSendPage.js
+++ src/pages/Token/TokenSendPage.js
@@ -1,15 +1,15 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { AppContext } from '../../AppProvider';
 import { useNavigation, withParams } from '../../routes/hooks';
 import { ROUTES_MAP } from '../../routes/app-routes';
-import { cache, CACHE_TYPES } from '../../utils/cache';
 import { withTranslation } from '../../hooks/useTranslations';
 import { LOGOS, getTransactionImage } from '../../utils/wallet';
 import { getMediaRemoteUrl } from '../../utils/media';
+import useToken from '../../hooks/useToken';
 
-import theme from '../../component-library/Global/theme';
+import theme, { globalStyles } from '../../component-library/Global/theme';
 import GlobalLayout from '../../component-library/Global/GlobalLayout';
 import GlobalBackTitle from '../../component-library/Global/GlobalBackTitle';
 import GlobalButton from '../../component-library/Global/GlobalButton';
@@ -20,7 +20,7 @@ import GlobalPadding from '../../component-library/Global/GlobalPadding';
 import GlobalText from '../../component-library/Global/GlobalText';
 import CardButtonWallet from '../../component-library/CardButton/CardButtonWallet';
 import IconCopy from '../../assets/images/IconCopy.png';
-import useToken from '../../hooks/useToken';
+import IconQRCodeScanner from '../../assets/images/IconQRCodeScanner.png';
 
 const styles = StyleSheet.create({
   buttonStyle: {
@@ -31,11 +31,6 @@ const styles = StyleSheet.create({
   },
   titleStyle: {
     color: theme.colors.labelTertiary,
-  },
-  addressBookItem: {
-    paddingVertical: 0,
-    paddingHorizontal: 0,
-    minHeight: 60,
   },
   centered: {
     justifyContent: 'center',
@@ -145,8 +140,10 @@ const TokenSendPage = ({ params, t }) => {
                 placeholder={`Name or ${'SOL'} Address`}
                 value={recipientAddress}
                 setValue={setRecipientAddress}
-                actionIcon="qr"
-                onActionPress={() => {}}
+                // actionIcon="qr"
+                // onActionPress={() => {}}
+                buttonIcon={IconQRCodeScanner}
+                buttonOnPress={() => {}}
               />
 
               <GlobalPadding />
@@ -163,8 +160,8 @@ const TokenSendPage = ({ params, t }) => {
                     chain={addressBookItem.chain}
                     imageSize="md"
                     onPress={() => setRecipientAddress(addressBookItem.address)}
-                    buttonStyle={styles.addressBookItem}
-                    touchableStyles={styles.touchableStyles}
+                    buttonStyle={globalStyles.addressBookItem}
+                    touchableStyles={globalStyles.addressBookTouchable}
                     transparent
                   />
                 ))}
@@ -178,8 +175,8 @@ const TokenSendPage = ({ params, t }) => {
                 value={recipientAmount}
                 setValue={setRecipientAmount}
                 keyboardType="numeric"
-                actionIcon="sendmax"
-                onActionPress={() => {}}
+                buttonLabel="Max"
+                buttonOnPress={() => {}}
               />
 
               <GlobalPadding />
