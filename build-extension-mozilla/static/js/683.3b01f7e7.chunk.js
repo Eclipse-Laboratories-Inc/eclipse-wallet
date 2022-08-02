8a72f29d2d51291972d66a563275e6a1d3548f5d
Dario Santos <daro@Darios-MacBook-Pro.local>
2022-08-02 16:18:58 -0300

    adding manifest por mozilla

---
+++ build-extension-mozilla/static/js/683.3b01f7e7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksalmon_app=self.webpackChunksalmon_app||[]).push([[683],{98683:function(a,p,s){s.r(p),p.default={}}}]);
+//# sourceMappingURL=683.3b01f7e7.chunk.js.map
