514e652a0609512eb93616fd1ae8ab0108ea655a
Dario Santos <daro@Darios-MacBook-Pro.local>
2022-08-10 11:55:54 -0300

    removing extension folder

--- build-extension-mozilla/static/js/683.3b01f7e7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksalmon_app=self.webpackChunksalmon_app||[]).push([[683],{98683:function(a,p,s){s.r(p),p.default={}}}]);
-//# sourceMappingURL=683.3b01f7e7.chunk.js.map
