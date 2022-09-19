2fe8fabe72403b111f721f0dc67db91db8b63ce6
Ezequiel Kruk <krukezequiel@gmail.com>
2022-09-19 11:52:39 -0300

    Password input validated (between 8 and 20 characters)

--- src/pages/Onboarding/CreateWalletPage.js
+++ src/pages/Onboarding/CreateWalletPage.js
@@ -235,8 +235,10 @@ const CreateWalletPage = ({ params, t }) => {
       setWaiting(false);
     }
   };
+
   const handleOnPasswordComplete = async password => {
     setWaiting(true);
+
     await addWallet(account, password, params.chainCode);
     setStep(5);
   };
