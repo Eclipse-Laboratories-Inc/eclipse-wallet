6be0de8f916f0c0bcdab033dd71802719903d5db
fran <francisco.leto@gmail.com>
2022-10-27 16:50:07 -0300

    replaced nfts slider mock data

--- src/component-library/Global/GlobalSlider.js
+++ src/component-library/Global/GlobalSlider.js
@@ -1,37 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import { useKeenSliderNative } from 'keen-slider/react-native';
-import theme, { globalStyles } from './theme';
-import GlobalText from './GlobalText';
-import GlobalImage from './GlobalImage';
-import GlobalButton from './GlobalButton';
-import GlobalPadding from './GlobalPadding';
-import IconExpandMore from '../../assets/images/IconExpandMore.png';
-import IconExpandLess from '../../assets/images/IconExpandLess.png';
-import IconHyperspace from '../../assets/images/IconHyperspace.jpeg';
-
-const { width: windowWidth } = Dimensions.get('window');
-import AppIcon from '../../assets/images/AppIcon.png';
+import theme from './theme';
 
 const styles = StyleSheet.create({
   sliderContainer: {
-    paddingBottom: 50,
-  },
-  image: {
-    marginRight: theme.gutters.paddingSM,
-  },
-  itemContainer: {
-    marginBottom: theme.gutters.paddingSM,
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  collapseButton: {
-    position: 'absolute',
-    width: '100%',
-    alignItems: 'center',
-    bottom: -20,
-    backgroundColor: theme.colors.bgPrimary,
+    paddingBottom: theme.gutters.paddingNormal,
   },
   dotsContainer: {
     alignItems: 'center',
@@ -62,99 +36,21 @@ const styles = StyleSheet.create({
 });
 
 const GlobalSlider = ({
-  // items,
-  // renderItem,
+  items,
+  slides,
+  renderItem,
+  sliderHeight,
   dots = true,
 }) => {
-  const [sliderHeight, setSliderHeight] = useState(294);
-  const [isCollapseOpen, setIsCollapseOpen] = useState(false);
   const [currentSlide, setCurrentSlide] = useState(0);
-  const slides = 3;
   const slider = useKeenSliderNative({ slides });
-
-  const sliderWidth = windowWidth - 28;
-
-  const [items] = useState([
-    {
-      title: 'Item 1',
-      text: 'Text 1',
-    },
-    {
-      title: 'Item 2',
-      text: 'Text 2',
-    },
-    {
-      title: 'Item 3',
-      text: 'Text 3',
-    },
-  ]);
-
-  const renderItem = ({ item, index }) => {
-    return (
-      <View
-        style={{
-          backgroundColor: theme.colors.bgPrimary,
-          borderRadius: theme.borderRadius.borderRadiusMD,
-          height: sliderHeight,
-          width: sliderWidth,
-          padding: theme.gutters.paddingSM,
-        }}>
-        <View style={globalStyles.inlineFlexButtons}>
-          <GlobalText type="body2">New collections</GlobalText>
-          <GlobalImage
-            circle
-            source={IconHyperspace}
-            size="xs"
-            style={globalStyles.centeredSmall}
-          />
-        </View>
-        <GlobalPadding size="sm" />
-        {Array(2).fill(
-          <View style={styles.itemContainer}>
-            <GlobalImage source={AppIcon} size="xxl" style={styles.image} />
-            <View>
-              <GlobalText type="body2">Collection Name</GlobalText>
-              <GlobalText type="caption">Collection Size Items</GlobalText>
-              <GlobalText type="caption">Floor: Floor_Price</GlobalText>
-              <GlobalText type="caption">1D Volume: Volume_1D</GlobalText>
-            </View>
-          </View>,
-        )}
-        {isCollapseOpen &&
-          Array(4).fill(
-            <View style={styles.itemContainer}>
-              <GlobalImage source={AppIcon} size="xxl" style={styles.image} />
-              <View>
-                <GlobalText type="body2">Collection Name</GlobalText>
-                <GlobalText type="caption">Collection Size Items</GlobalText>
-                <GlobalText type="caption">Floor: Floor_Price</GlobalText>
-                <GlobalText type="caption">1D Volume: Volume_1D</GlobalText>
-              </View>
-            </View>,
-          )}
-        <GlobalButton
-          type="icon"
-          transparent
-          icon={isCollapseOpen ? IconExpandLess : IconExpandMore}
-          onPress={toggleCollapse}
-          size="medium"
-          style={styles.collapseButton}
-        />
-      </View>
-    );
-  };
-
-  const toggleCollapse = t => {
-    setSliderHeight(isCollapseOpen ? 294 : 740);
-    setIsCollapseOpen(!isCollapseOpen);
-  };
 
   return (
     <View style={styles.sliderContainer}>
       <View style={{ height: sliderHeight }} {...slider.containerProps}>
         {items?.map((item, index) => (
           <View key={index} {...slider.slidesProps[index]}>
-            {renderItem(item, index)}
+            {renderItem(item)}
           </View>
         ))}
       </View>
@@ -163,7 +59,6 @@ const GlobalSlider = ({
           {items?.map(idx => (
             <TouchableOpacity
               onPress={() => {
-                console.log('idx', idx);
                 slider.moveToSlideRelative(idx);
               }}
               style={
--- src/pages/Nfts/NftsListPage.js
+++ src/pages/Nfts/NftsListPage.js
@@ -1,5 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View } from 'react-native';
+import {
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
 
 import { AppContext } from '../../AppProvider';
 import { useNavigation } from '../../routes/hooks';
@@ -10,25 +16,65 @@ import { ROUTES_MAP as NFTS_ROUTES_MAP } from './routes';
 import { getWalletName } from '../../utils/wallet';
 import { isMoreThanOne } from '../../utils/nfts';
 
-import { globalStyles } from '../../component-library/Global/theme';
+import theme, { globalStyles } from '../../component-library/Global/theme';
 import GlobalSkeleton from '../../component-library/Global/GlobalSkeleton';
 import GlobalLayout from '../../component-library/Global/GlobalLayout';
 import GlobalNftList from '../../component-library/Global/GlobalNftList';
 import GlobalText from '../../component-library/Global/GlobalText';
 import GlobalSlider from '../../component-library/Global/GlobalSlider';
+import GlobalImage from '../../component-library/Global/GlobalImage';
+import GlobalButton from '../../component-library/Global/GlobalButton';
+import GlobalPadding from '../../component-library/Global/GlobalPadding';
+import IconExpandMore from '../../assets/images/IconExpandMore.png';
+import IconExpandLess from '../../assets/images/IconExpandLess.png';
+import IconHyperspace from '../../assets/images/IconHyperspace.jpeg';
 import Header from '../../component-library/Layout/Header';
+import AppIcon from '../../assets/images/AppIcon.png';
 
 import useAnalyticsEventTracker from '../../hooks/useAnalyticsEventTracker';
 import { SECTIONS_MAP } from '../../utils/tracking';
+const { width: windowWidth } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  image: {
+    marginRight: theme.gutters.paddingSM,
+    aspectRatio: 1,
+    backgroundColor: theme.colors.bgLight,
+    borderRadius: theme.borderRadius.borderRadiusXL,
+    overflow: 'hidden',
+    zIndex: 1,
+  },
+  itemContainer: {
+    marginBottom: theme.gutters.paddingSM,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  collapseButton: {
+    position: 'absolute',
+    width: '100%',
+    alignItems: 'center',
+    bottom: -20,
+    backgroundColor: theme.colors.bgPrimary,
+  },
+});
 
 const NftsListPage = ({ t }) => {
   useAnalyticsEventTracker(SECTIONS_MAP.NFT_LIST);
   const navigate = useNavigation();
+  const [{ activeWallet, config }] = useContext(AppContext);
   const [loaded, setLoaded] = useState(false);
   const [nftsGroup, setNftsGroup] = useState([]);
   const [listedInfo, setListedInfo] = useState([]);
+  const [sliderHeight, setSliderHeight] = useState(294);
+  const [isCollapseOpen, setIsCollapseOpen] = useState(false);
+  // const [trendingCollections, setTrendingCollections] = useState([]);
+  // const [newCollections, setNewCollections] = useState([]);
+  // const [launchpadCollections, setLaunchpadCollections] = useState([]);
+  const [sliderItems, setSliderItems] = useState([]);
 
-  const [{ activeWallet, config }] = useContext(AppContext);
+  const sliderWidth = windowWidth - 28;
+
   useEffect(() => {
     if (activeWallet) {
       cache(
@@ -39,6 +85,13 @@ const NftsListPage = ({ t }) => {
         setNftsGroup(nfts);
         const listed = await activeWallet.getListedNfts();
         setListedInfo(listed);
+        const trendColls = await activeWallet.getCollectionGroup('trending');
+        setSliderItems([
+          {
+            title: 'Trending collections',
+            value: trendColls?.project_stats?.splice(0, 6),
+          },
+        ]);
         setLoaded(true);
       });
     }
@@ -56,6 +109,112 @@ const NftsListPage = ({ t }) => {
     }
   };
 
+  const openCollection = async project_id => {
+    const url = `https://hyperspace.xyz/collection//${project_id}`;
+    const supported = await Linking.canOpenURL(url);
+    if (supported) {
+      await Linking.openURL(url);
+    } else {
+      console.log(`UNSUPPORTED LINK ${url}`);
+    }
+  };
+
+  const renderItem = ({ title, value }) => (
+    <View
+      style={{
+        backgroundColor: theme.colors.bgPrimary,
+        borderRadius: theme.borderRadius.borderRadiusMD,
+        height: sliderHeight,
+        width: sliderWidth,
+        padding: theme.gutters.paddingSM,
+      }}>
+      <View style={globalStyles.inlineFlexButtons}>
+        <GlobalText type="body2">{title}</GlobalText>
+        <GlobalImage
+          circle
+          source={IconHyperspace}
+          size="xs"
+          style={globalStyles.centeredSmall}
+        />
+      </View>
+      <GlobalPadding size="sm" />
+      {value?.slice(0, 2).map(coll => (
+        <TouchableOpacity onPress={() => openCollection(coll.project_id)}>
+          <View style={styles.itemContainer}>
+            <GlobalImage
+              source={coll.project.img_url}
+              size="xxl"
+              style={styles.image}
+            />
+            <View>
+              <GlobalText type="body2">{coll.project.display_name}</GlobalText>
+              <GlobalText type="caption">
+                {coll.project.supply} Items
+              </GlobalText>
+              <GlobalText type="caption">Floor: {coll.floor_price}</GlobalText>
+              <GlobalText type="caption">
+                1D Volume: {coll.volume_1day} SOL
+              </GlobalText>
+            </View>
+          </View>
+        </TouchableOpacity>
+      ))}
+      {isCollapseOpen &&
+        value?.slice(2, 6).map(coll => (
+          <TouchableOpacity onPress={() => openCollection(coll.project_id)}>
+            <View style={styles.itemContainer}>
+              <GlobalImage
+                source={coll.project.img_url}
+                size="xxl"
+                style={styles.image}
+              />
+              <View>
+                <GlobalText type="body2">
+                  {coll.project.display_name}
+                </GlobalText>
+                <GlobalText type="caption">
+                  {coll.project.supply} Items
+                </GlobalText>
+                <GlobalText type="caption">
+                  Floor: {coll.floor_price}
+                </GlobalText>
+                <GlobalText type="caption">
+                  1D Volume: {coll.volume_1day} SOL
+                </GlobalText>
+              </View>
+            </View>
+          </TouchableOpacity>
+        ))}
+      <GlobalButton
+        type="icon"
+        transparent
+        icon={isCollapseOpen ? IconExpandLess : IconExpandMore}
+        onPress={toggleCollapse}
+        size="medium"
+        style={styles.collapseButton}
+      />
+    </View>
+  );
+
+  const toggleCollapse = t => {
+    setSliderHeight(isCollapseOpen ? 294 : 740);
+    setIsCollapseOpen(!isCollapseOpen);
+  };
+
+  // const items = [
+  //   {
+  //     title: 'Trending collections',
+  //     value: trendingCollections,
+  //   },
+  //   {
+  //     title: 'New collections',
+  //     value: newCollections,
+  //   },
+  //   {
+  //     title: 'Launchpad',
+  //     value: launchpadCollections,
+  //   },
+  // ];
   return (
     (
       <GlobalLayout>
@@ -63,9 +222,20 @@ const NftsListPage = ({ t }) => {
           <GlobalLayout.Header>
             <Header activeWallet={activeWallet} config={config} t={t} />
             <View style={globalStyles.centered}>
+              <GlobalText type="headline2">{t(`wallet.nfts`)}</GlobalText>
+            </View>
+            {sliderItems?.length && (
+              <GlobalSlider
+                items={sliderItems.filter(({ value }) => value.length)}
+                // slides={items.filter(({ value }) => value).length}
+                slides={sliderItems.length}
+                renderItem={renderItem}
+                sliderHeight={sliderHeight}
+              />
+            )}
+            <View style={globalStyles.centered}>
               <GlobalText type="headline2">{t(`wallet.my_nfts`)}</GlobalText>
             </View>
-            <GlobalSlider />
             <GlobalNftList
               nonFungibleTokens={nftsGroup}
               listedInfo={listedInfo}
