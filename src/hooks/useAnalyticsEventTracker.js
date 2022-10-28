6121c76ad88cc8224e1d968c5b5aeb4290bc1938
salmon-h <hernando@salmonwallet.io>
2022-10-28 18:10:22 -0300

    Disable tracking for extension

--- src/hooks/useAnalyticsEventTracker.js
+++ src/hooks/useAnalyticsEventTracker.js
@@ -1,17 +1,22 @@
 import { useEffect } from 'react';
 import ReactGA from 'react-ga4';
+import { isExtension } from '../utils/platform';
 
 const useAnalyticsEventTracker = section => {
   useEffect(() => {
-    ReactGA.send({ hitType: 'pageview', page: section });
+    if (!isExtension()) {
+      ReactGA.send({ hitType: 'pageview', page: section });
+    }
   }, [section]);
 
   const trackEvent = (action, label) => {
-    ReactGA.event({
-      category: section,
-      action: action,
-      label: label,
-    });
+    if (!isExtension()) {
+      ReactGA.event({
+        category: section,
+        action: action,
+        label: label,
+      });
+    }
   };
   return { trackEvent };
 };
