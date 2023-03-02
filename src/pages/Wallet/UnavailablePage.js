bb2ac719de59461925d3ec4a0cdf658f0f9e6c03
fran <francisco.leto@gmail.com>
2023-03-02 09:48:23 -0300

    fixing UnavailablePage text

--- src/pages/Wallet/UnavailablePage.js
+++ src/pages/Wallet/UnavailablePage.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import { withTranslation } from '../../hooks/useTranslations';
 import GlobalLayout from '../../component-library/Global/GlobalLayout';
 import GlobalBackTitle from '../../component-library/Global/GlobalBackTitle';
 import GlobalPadding from '../../component-library/Global/GlobalPadding';
 import GlobalText from '../../component-library/Global/GlobalText';
 import Header from '../../component-library/Layout/Header';
 
-const UnavailablePage = () => {
+const UnavailablePage = ({ t }) => {
   return (
     <GlobalLayout>
       <GlobalLayout.Header>
         <Header />
-        <GlobalBackTitle title="Not available" />
+        <GlobalBackTitle title={t('wallet.not_available_title')} />
 
         <GlobalPadding />
         <GlobalText type={'body1'} center>
-          Not available
+          {t('wallet.not_available_msg')}
         </GlobalText>
       </GlobalLayout.Header>
     </GlobalLayout>
   );
 };
 
-export default UnavailablePage;
+export default withTranslation()(UnavailablePage);
